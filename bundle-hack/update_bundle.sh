6455f756c3eb5632419b0991ab84ad5f9010751b
Yorgos Saslis <yorgos.saslis@redhat.com>
2024-12-05 08:30:03 +0200

    fails script in case of error
    not cool for this script to not fail when substitutions aren't made, innit ?
    
    Signed-off-by: Yorgos Saslis <yorgos.saslis@redhat.com>

--- bundle-hack/update_bundle.sh
+++ bundle-hack/update_bundle.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# enables strict mode: `-e` fails if error, `-u` checks variable references, `-o pipefail`: prevents errors in a pipeline from being masked
+set -euo pipefail
+
 export BACKEND_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/3scale-prod-tenant/apisonator@sha256:af6defac45e66b010b75bfcafac54ff7656eb0acfb60f84d682cbd2a5737f253"
 export APICAST_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/3scale-prod-tenant/apicast-gateway@sha256:783ce38427e53bcfce0bc548e35cd1283fada7b708fdb3c57a6fd0ac73603b61"
 export SYSTEM_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/3scale-prod-tenant/porta@sha256:d08d7d6c98d1b922c50aa9dbb3ee636a41abb989c00e4d39eaf4b85c168cf034"
