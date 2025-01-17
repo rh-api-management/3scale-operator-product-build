79455703fe32601c300500390c94ca881172dc15
Yorgos Saslis <yorgos.saslis@redhat.com>
2025-01-17 08:49:18 +0200

    updates to latest valid image references
    Signed-off-by: Yorgos Saslis <yorgos.saslis@redhat.com>

--- bundle-hack/update_bundle.sh
+++ bundle-hack/update_bundle.sh
@@ -3,10 +3,10 @@
 # enables strict mode: `-e` fails if error, `-u` checks variable references, `-o pipefail`: prevents errors in a pipeline from being masked
 set -euo pipefail
 
-export BACKEND_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/3scale-prod-tenant/apisonator@sha256:af6defac45e66b010b75bfcafac54ff7656eb0acfb60f84d682cbd2a5737f253"
+export BACKEND_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/3scale-prod-tenant/apisonator@sha256:c05da822e8239b40d6898e7d970ea9901d895b379958c078a177524bef077845"
 export APICAST_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/3scale-prod-tenant/apicast-gateway@sha256:de202ee9c78ae42a8a315563f8257295b4c97402dae72adad93375cdb2196ed7"
 export SYSTEM_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/3scale-prod-tenant/porta@sha256:d08d7d6c98d1b922c50aa9dbb3ee636a41abb989c00e4d39eaf4b85c168cf034"
-export ZYNC_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/3scale-prod-tenant/zync@sha256:bf3d99b1a6b845802b53d1067d9b866d3688efcc1f90a6e791f78111c1d14c8e"
+export ZYNC_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/3scale-prod-tenant/zync@sha256:c175ff53cfc8c1c4383d9ffc3a2972b8e6f424d4d510b93daa6566b476834db2"
 export SEARCHD_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/3scale-prod-tenant/searchd@sha256:010fe292d7500b426852627a63ebb2e7720cb7383d4d958a6d6b7fb2619a1aa6"
 
 # non-3scale dependencies
